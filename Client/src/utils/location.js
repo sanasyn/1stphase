8ca21e9cc8ab7987a160e1e2b368dffaa7702ab2
Whitney Wong <whitneywong@Whitneys-MacBook-Pro.local>
2018-03-01 20:33:09 -0600

    More changes

--- Client/src/utils/location.js
+++ Client/src/utils/location.js
@@ -2,7 +2,21 @@ import axios from 'axios';
 const APIKEY = require('../../../config/config').googlemaps;
 
 const locationHelper = {
-    
+    getFacilityDistance: (zip, facilities) => {
+        const modifiedObj = facilities.map((facility) => {
+            const facilityAddress = facility.city+','+facility.state+','+facility.zip;
+
+            return axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${zip}&destinations=${facilityAddress}&key=${APIKEY}`)
+                .then((res) => {
+                    let miles = (res.data.rows[0].elements[0].distance.text).slice(0,-3);
+                    facility.distance = parseInt(miles.replace(/,/g,''));
+
+                    return facility
+                })
+        })
+
+        return Promise.all(modifiedObj)
+    }
 }
 
 // const getFacilityDistance = (zip, facilities) => {
