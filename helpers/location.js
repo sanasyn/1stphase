58b5d38f8f1cbde6ebd5b0ce943a6e00e6c76bbc
Whitney Wong <whitneywong@Whitneys-MacBook-Pro.local>
2018-03-05 22:35:52 -0600

    Distance calculation using zipcode table

--- helpers/location.js
+++ helpers/location.js
@@ -1,39 +1,81 @@
-const axios = require('axios');
-const APIKEY = require('../config/config').googlemaps;
+const config = require('../config/config').zipcode;
+const pg = require('pg');
+const knex = require('knex')(getConnectionOptions());
 
-const getFacilityDistance = (zip, facilities) => {
-    const addressArray =[];
-    // facilities.map((facility) => {
-    //     if (facility.state && !isNaN(facility.zip) && facility.zip.length === 5) {
-    //         addressArray.push(facility);
-    //     }
-    // })
+function getConnectionOptions() {
+	return {
+		client: config.client,
+		connection : {
+			host: config.host,
+			user: config.user,
+			password: config.password,
+			database: config.database,
+			ssl: true
+		}
+	}
+}
+
+// Main function that invokes helper functions
+function getDistance(source, facilities){
+    return getSourceZip(source)
+        .then((sourceZip) => facilityDistance(sourceZip, facilities))
+        .then((facility) => {
+            return facility.sort((a,b) => a.distance - b.distance)
+        })
+        .catch((err) => console.log(err))
+}
+
+// Function to return lat and long using user's zipcode
+function getSourceZip(source){
+    return knex
+        .select('zipcode', 'latitude', 'longitude')
+        .from('zipcodes')
+        .where('zipcode', source)
+        .limit(10)
+        .then((row) => {
+            return row[0];
+        })
+        .catch((err) => console.log(err))
+}
+
+// Map through facility results and perform math calculation for distance
+function facilityDistance(source, facilities){
     const modifiedObj = facilities.map((facility) => {
-        const facilityAddress = constructAddress(facility);
-        console.log("FACILITY ADDRESS: ", facilityAddress);
-
-        return axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${zip}&destinations=${facilityAddress}&key=${APIKEY}`)
-            .then((res) => {
-                // console.log("RES: ",res);
-                let miles = res.data.rows[0].elements[0].status === "OK" ? (res.data.rows[0].elements[0].distance.text).slice(0,-3) : "100000";
-                facility.distance = parseInt(miles.replace(/,/g,''));
-
+        return knex
+                .select('zipcode', 'latitude', 'longitude')
+                .from('zipcodes')
+                .where('zipcode', facility.zip)
+                .limit(10)
+                .then((row) => {
+                    return row[0];
+            })
+            .then((dest) => doMath(source, dest))
+            .then((dist) => {
+                facility.distance = dist;
                 return facility
             })
     })
 
     return Promise.all(modifiedObj)
-    .then(values => {
-        return values.sort((a,b) => a.distance - b.distance)
-    })
-    .catch(error => {
-        console.log(error.error)
-        return error.error
-    })
 }
 
-function constructAddress(facility){
-    return facility.city+','+facility.state+','+facility.zip
+// Math calculations using the haversine formula: https://www.movable-type.co.uk/scripts/latlong.html
+function doMath(source, dest){
+    const earthRadius = 3959;
+    const sourceLat = parseFloat(source.latitude);
+    const sourceLong = parseFloat(source.longitude);
+    const destLat = parseFloat(dest.latitude);
+    const destLong = parseFloat(dest.longitude);
+    const sourceLatRad = sourceLat*Math.PI/180;
+    const destLatRad = destLat*Math.PI/180;
+    const deltaLatRad = (destLat - sourceLat)*Math.PI/180;
+    const deltaLongRad = (destLong - sourceLong)*Math.PI/180;
+
+    const a = Math.sin(deltaLatRad/2) * Math.sin(deltaLatRad/2) + Math.cos(sourceLatRad) * Math.cos(destLatRad) * Math.sin(deltaLongRad/2) * Math.sin(deltaLongRad/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    const distance = earthRadius * c;
+    return Math.round(distance);
 }
 
-module.exports = getFacilityDistance;
+module.exports = getDistance;
