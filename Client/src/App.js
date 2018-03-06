05dcc703ed9be0d5604b6cf438420ef7fc9bad10
Marissa Pels <marissapels@Marissas-MacBook-Pro.local>
2018-03-05 22:46:45 -0600

    change color

--- Client/src/App.js
+++ Client/src/App.js
@@ -611,7 +611,7 @@ handleClickBack() {
       <div>
       <MuiThemeProvider>
         <header style={{marginTop: "0", backgroundColor: "#20759c",minHeight:'255',borderTop: "solid #545454 8px"}}>
-          <h1 style={{marginTop: "0", padding: "6px", color: "#fff", fontFamily:"Acme", fontSize: "3.0em"}}><span style={{fontFamily: "Arial", letterSpacing: "-6px", fontSize: "0.7em", paddingRight: "14px", fontStyle: "italic", color: "black"}}>S&#423;</span>SanaSyn</h1>
+          <h1 style={{marginTop: "0", padding: "6px", color: "#fff", fontFamily:"Acme", fontSize: "3.0em"}}><span style={{fontFamily: "Arial", letterSpacing: "-6px", fontSize: "0.7em", paddingRight: "14px", fontStyle: "italic", color: "#CCC515"}}>S&#423;</span>SanaSyn</h1>
           {/* <img src="./SanaSynTitle.svg" alt="SanaSyn" alt="SanaSyn"/> */}
         </header>
 
