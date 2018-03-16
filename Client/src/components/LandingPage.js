b8fbd2fbc88ff0e927cc732a1b3d167ebdb63944
Marissa Pels <marissapels@Marissas-MacBook-Pro.local>
2018-03-15 21:50:13 -0500

    updates to landing page

--- Client/src/components/LandingPage.js
+++ Client/src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Background from '../synapse.jpg';
-// import Latin from '../latin.png';
+import Latin from '../latin.png';
 import FlatButton from 'material-ui/FlatButton';
 import { Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -17,58 +17,35 @@ export default () => {
 
     return (
     	<div>
-
     		<div className="topStyle row">
     			<Image className="col-md-6" circle src={Background}/>
-    			<div className="col-md-6">
+    			<div className="col-md-4 col-md-offset-1" >
 	    			<h1 className="healingConn">A Healing Connection</h1>
-	 		        <p className="startInfo">We connect you to clinical trials for Alzheimer's based on your needs.</p>
+	 		        <p className="startInfo">We connect you to clinical trials for Alzheimer's Disease based on your needs.</p>
 	 		        <Link to="/quiz">
  		        			<FlatButton style={StyleButton}>Get Started
  		        			</FlatButton>
  		        		</Link>
 	 		    </div>
     		</div>
+        <div className="latin">
+          < Image className="col-md-offset-3 col-md-6 latinImage" src={Latin}/>
+        </div>
+        <div className="stepContainer col-md-offset-3 col-md-6">
+          <div className="step">
+            <h2 className="stepNumber">1</h2>
+            <p className="stepDesc">Click Get Started in order to begin a short questionaire.</p>
+          </div>
+          <div className="step">
+            <h2 className="stepNumber">2</h2>
+            <p className="stepDesc">We will return a list of studies that fit your needs in a location close to you.</p>
+          </div>
+          <div className="step">
+            <h2 className="stepNumber">3</h2>
+            <p className="stepDesc">Click on a study to learn more about it and receive contact information for the facility.</p>
+          </div>
+        </div>
     	</div>
-// {
-//         <div>
-// 	        <div className="topStyle">
-// 	        	<img className="synapseImage" src={Background}/>
-// 	        		<div className="containDiv">
-// 		        		<h1 className="healingConn">A Healing Connection</h1>
-// 		        		<p className="startInfo">We connect you to clinical trials for Alzheimer's based on your needs.</p>
-// 		        		
-// 	        		</div>
-// 	        </div>
-// 	        <div className="underTopOut">
-// 	        	<div className="underTopIn">
-// 	        		<img className="latin" src={Latin}/>
-// 	        	</div>
-// 	        </div>
-// 	        <div className="underTopOut2">
-// 	        	<div className="underTopIn2"></div>
-// 	        </div>
-// 	        <div className="bottomStyle">
-// 	        		<div className="containDiv2">
-// 	        			<h1>Why SanaSyn?</h1>
-// 	        			<p>Because finding the right clinical trial should be easy.</p>
-// 	        			<h1>What do you have to do?</h1>
-// 	        			<p>Answer questions we have tailored specifically to show you trials most relevant and closest to your location.</p>
-// 	        			<h1>Why is this search engine different?</h1>
-// 	        			<p>We won't show you studies that you don't qualify for</p>
 
-// 	        		</div>
-// 	        </div>
-// 	    </div>
-    	// 		<div className="topStyle">
-    	// 		<Image circle style={{display:"block", width:"50%"}} src={Background}/>
-    	// 		<div>
-	    // 			<h1 className="healingConn">A Healing Connection</h1>
-	 		 //        		<p className="startInfo">We connect you to clinical trials for Alzheimer's based on your needs.</p>
-	 		 //    </div>
-    	// 	</div>
-
-
-// 	}
     )
 }
