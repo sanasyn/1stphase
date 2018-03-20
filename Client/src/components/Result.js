892156b3ab54d08d97430a46c74605a4eff0c9b9
Marissa Pels <marissapels@cpe-172-100-3-51.twcny.res.rr.com>
2018-03-19 20:48:52 -0500

    pageination

--- Client/src/components/Result.js
+++ Client/src/components/Result.js
@@ -21,22 +21,30 @@ class Result extends Component {
             showResults: true,
             showDetails: false,
             study: '',
-            contact: ''
+            contact: '',
+            currentPage: 1,
+            resultsPerPage: 25,
         }
 
         this.onStudySelect=this.onStudySelect.bind(this)
+    }
+    resultsOnPage() {
+      const indexOfLastResult = this.state.currentPage * this.state.resultsPerPage;
+      const indexOfFirstResult = indexOfLastResult - this.state.resultsPerPage;
+      const results = this.props.results.slice(indexOfFirstResult, indexOfLastResult);
+      return results;
     }
 
     onStudySelect(study) {
         console.log("in on Study Select",study);
         this.setState({showDetails: true, showResults: false})
         axios.post("/resultDetails", study)
-            .then((results) => {
-                this.setState({ 
-                    study: results.data.study[0],
-                    contact: results.data.contact[0]
-                })
+          .then((results) => {
+            this.setState({ 
+              study: results.data.study[0],
+              contact: results.data.contact[0]
             })
+          })
     }
 
     goBack() {
@@ -46,44 +54,60 @@ class Result extends Component {
         })
     }
 
+    previousPageNumber() {
+      this.setState({currentPage: this.state.currentPage - 1})
+    }
+
+    nextPageNumber() {
+      this.setState({currentPage: this.state.currentPage + 1})
+    }
+
     render(){
         const resultTable = (   
-            <Table style={{maxWidth:"90%", margin:"auto"}}>
-                <TableHeader displaySelectAll={false}>
-                    <TableRow>
-                        <TableHeaderColumn style={{paddingLeft: '0px'}}>See Details</TableHeaderColumn>
-                        <TableHeaderColumn style={{paddingLeft: '0px'}}>NCT_ID</TableHeaderColumn>
-                        <TableHeaderColumn style={{paddingLeft: '0px'}}>Title</TableHeaderColumn>
-                        <TableHeaderColumn style={{paddingLeft: '0px'}}>City, State</TableHeaderColumn>
-                        <TableHeaderColumn style={{paddingLeft: '0px'}}>Zip Codes</TableHeaderColumn>
-                    </TableRow>
-                </TableHeader>
-                <TableBody displayRowCheckbox={false}>
-                    {this.props.results.map((study, i) => {
-                        return (
-                            <TableRow key={i}>
-                                <TableRowColumn><FlatButton style={{backgroundColor: "#6ab6c5", hoverColor: "#b8e2ea", marginTop:"20px"}}  onClick={this.onStudySelect.bind(this, study)}>Details</FlatButton></TableRowColumn>
-                                <TableRowColumn>{study.nct_id}</TableRowColumn>
-                                <TableRowColumn>{study.official_title}</TableRowColumn>
-                                <TableRowColumn>{study.city}, {study.state}</TableRowColumn>
-                                <TableRowColumn>{study.zip}</TableRowColumn>
-                            </TableRow>
-                        )
-                    }
-                    )}
-                </TableBody>
-            </Table>
-            )
+          <Table style={{maxWidth:"90%", margin:"auto"}}>
+            <TableHeader displaySelectAll={false}>
+              <TableRow>
+                <TableHeaderColumn style={{paddingLeft: '0px', width: '12%', fontSize: '1.3em'}}>See Details</TableHeaderColumn>
+                <TableHeaderColumn style={{paddingLeft: '0px', width: '12%', fontSize: '1.3em'}}>NCT_ID</TableHeaderColumn>
+                <TableHeaderColumn style={{paddingLeft: '0px', width: '51%', fontSize: '1.3em'}}>Title</TableHeaderColumn>
+                <TableHeaderColumn style={{paddingLeft: '0px', width: '15%', fontSize: '1.3em'}}>City, State</TableHeaderColumn>
+                <TableHeaderColumn style={{paddingLeft: '0px', width: '12%', fontSize: '1.3em'}}>Zip Codes</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+              {this.resultsOnPage().map((study, i) => {
+                return (
+                  <TableRow key={i}>
+                    <TableRowColumn><FlatButton style={{backgroundColor: "#6ab6c5", hoverColor: "#b8e2ea", marginTop:"20px", width: '10%', fontSize: '1.3em'}}  onClick={this.onStudySelect.bind(this, study)}>Details</FlatButton></TableRowColumn>
+                    <TableRowColumn style={{width: '12%', fontSize: '1.3em'}}>{study.nct_id}</TableRowColumn>
+                    <TableRowColumn style={{width: '51%', whiteSpace: 'normal', fontSize: '1.3em'}}>{study.official_title}</TableRowColumn>
+                    <TableRowColumn style={{width: '15%', fontSize: '1.3em'}}>{study.city}, {study.state}</TableRowColumn>
+                    <TableRowColumn style={{width: '12%', fontSize: '1.3em'}}>{study.zip}</TableRowColumn>
+                  </TableRow>
+                )
+              }
+              )}
+            </TableBody>
+          </Table>
+          )
 
         const detailPage = (
             <ResultDetail study={this.state.study} contact={this.state.contact} back={() => this.goBack()}/>
         )
 
         return (
-            <div>
-                {this.state.showResults ? resultTable : null }
-                {this.state.showDetails ? detailPage : null }
+          <div className="resultsPage">
+            {this.state.showResults ? resultTable : null }
+            {this.state.showDetails ? detailPage : null }
+            <div className="pagenation" style={{textAlign: "center"}}>
+              {this.state.currentPage > 1 ? (
+                  <FlatButton onClick={() => {this.previousPageNumber()}}><i className="glyphicon glyphicon-triangle-left" /></FlatButton>
+                ): null}
+              <div className="currentPage" style={{display:"inline-block", fontWeight:"bold", fontSize:"25px", position:"absolute", left: "49.5%"}}>{this.state.currentPage}</div>
+              <FlatButton onClick={() => {this.nextPageNumber()}}><i className="glyphicon glyphicon-triangle-right" /></FlatButton>
             </div>
+          </div>
+
         )
     } 
 }
