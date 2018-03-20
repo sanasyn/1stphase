aa7f11fc5e79118a5838be31f87ccbe259450ec1
Whitney Wong <whitneywong@cpe-172-100-3-197.twcny.res.rr.com>
2018-03-19 19:59:10 -0500

    Updated styles to details page.

--- Client/src/components/ResultDetail.js
+++ Client/src/components/ResultDetail.js
@@ -1,58 +1,65 @@
 import React, { Component } from 'react';
 import FlatButton from 'material-ui/FlatButton';
+import {Card, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {List, ListItem} from 'material-ui/List';
+import FontIcon from 'material-ui/FontIcon';
 
 class ResultDetail extends Component {
   render(){
     return (
       <div className="row detail-container">
-        <div className="col-md-12 detail-title">
-          {this.props.study.official_title}
-        </div>
-        
-        <div className="col-md-12 detail-description">
-          <h4>Brief Summary</h4>
-          <p>{this.props.study.description}</p>
-        </div>
+        <Card className="col-md-12 detail-topsection">
+          <CardTitle className="detail-study-title">{this.props.study.official_title}</CardTitle>
+          <CardText className="detail-description" style={{fontSize:'1.2em'}}>{this.props.study.description}</CardText>
+        </Card>
 
         <div className="row detail-midsection">
-          <div className="col-md-4 detail-phase">
-            <h5>Trial Phase</h5>
-            {this.props.study.phase}
-          </div>
-          <div className="col-md-4 detail-eligibility">
-            <h5>Eligibility Section</h5>
-          </div>
-          <div className="col-md-4 detail-studylink">
+          <Card div className="col-md-4 detail-phase">
+            <CardTitle className="detail-title">Trial Phase</CardTitle>
+            <CardText className="detail-study-phase" style={{fontSize: '2.5em'}}>{this.props.study.phase}</CardText>
+          </Card>
+          <Card className="col-md-4 detail-eligibility">
+            <CardTitle className="detail-title">Eligibility Section</CardTitle>
+          </Card>
+          <Card className="col-md-4 detail-studylink">
             <a target="_blank" href={`https://clinicaltrials.gov/ct2/show/${this.props.study.nct_id}`}>
               Clinical Trials Page for {this.props.study.nct_id}
             </a>
-          </div>
+          </Card>
         </div>
 
         <div className="row detail-endsection">
-          <div className="col-md-5 detail-contact">
-            <h5>Contact Information</h5>
-            <p>Facility Contact
-              <li>Name: {this.props.contact.facility_contact_name === null ? 'N/A' : this.props.contact.facility_contact_name}</li>
-              <li>Email: {this.props.contact.facility_contact_email === null ? 'N/A' : this.props.contact.facility_contact_email}</li>
-              <li>Phone: {this.props.contact.facility_contact_phone === null ? 'N/A' : this.props.contact.facility_contact_phone}</li>
-            </p>
-            <p>Central Contact
-              <li>Name: {this.props.contact.central_contact_name === null ? 'N/A' : this.props.contact.central_contact_name}</li>
-              <li>Email: {this.props.contact.central_contact_email === null ? 'N/A' : this.props.contact.central_contact_email}</li>
-              <li>Phone: {this.props.contact.central_contact_phone === null ? 'N/A' : this.props.contact.central_contact_phone}</li>
-            </p>
-          </div>
-          <div className="col-md-7 detail-map">
-          <iframe
-            title={"result-map"}
-            width={"600"}
-            height={"450"}
-            frameBorder={"0"}
-            src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBCyRuFxxuSVcYNNDZmVWrBUJgHaoXhLJ0&q=${this.props.study.city},${this.props.study.state},${this.props.study.zip}`}
-          >
-          </iframe>
-          </div>
+          <Card className="col-md-6 detail-contact">
+            <CardTitle className="detail-title">Contact Information</CardTitle>
+            <CardText>
+              <CardTitle style={{padding: '0px'}} subtitleStyle={{fontSize: '1.5em'}} subtitle="Facility Contact" />
+              <List>
+                <ListItem innerDivStyle={{padding: '3px'}}><FontIcon className="material-icons">person</FontIcon> {this.props.contact.facility_contact_name === null ? 'N/A' : this.props.contact.facility_contact_name}</ListItem>
+                <ListItem innerDivStyle={{padding: '3px'}}><FontIcon className="material-icons">email</FontIcon> {this.props.contact.facility_contact_email === null ? 'N/A' : this.props.contact.facility_contact_email}</ListItem>
+                <ListItem innerDivStyle={{padding: '3px'}}><FontIcon className="material-icons">phone</FontIcon> {this.props.contact.facility_contact_phone === null ? 'N/A' : this.props.contact.facility_contact_phone}</ListItem>
+              </List>
+            </CardText>
+            <CardText>
+              <CardTitle style={{padding: '0px'}} subtitleStyle={{fontSize: '1.5em'}} subtitle="Central Contact" />
+              <List>
+                <ListItem innerDivStyle={{padding: '3px'}}><FontIcon className="material-icons">person</FontIcon> {this.props.contact.central_contact_name === null ? 'N/A' : this.props.contact.central_contact_name}</ListItem>
+                <ListItem innerDivStyle={{padding: '3px'}}><FontIcon className="material-icons">email</FontIcon> {this.props.contact.central_contact_email === null ? 'N/A' : this.props.contact.central_contact_email}</ListItem>
+                <ListItem innerDivStyle={{padding: '3px'}}><FontIcon className="material-icons">phone</FontIcon> {this.props.contact.central_contact_phone === null ? 'N/A' : this.props.contact.central_contact_phone}</ListItem>
+              </List>
+            </CardText>
+          </Card>
+          <Card className="col-md-6 detail-map">
+            <CardMedia>
+              <iframe
+                title={"result-map"}
+                width={"600"}
+                height={"450"}
+                frameBorder={"0"}
+                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBCyRuFxxuSVcYNNDZmVWrBUJgHaoXhLJ0&q=${this.props.study.city},${this.props.study.state},${this.props.study.zip}`}
+              >
+              </iframe>
+          </CardMedia>
+          </Card>
         </div>
         
         <div className="row detail-back-row">
